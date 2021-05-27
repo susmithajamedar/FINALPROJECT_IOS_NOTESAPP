446b504cd5ac937ff9d6c6606e57bffd78e23cd0
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-27 16:06:03 -0400

    Added variables in Class for Action outlets
    Added variables in Class for Action outlets

--- note_avengers_iOS/note_avengers_iOS/LocationPickerViewController.swift
+++ note_avengers_iOS/note_avengers_iOS/LocationPickerViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-
+import MapKit
 class LocationPickerViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var addressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +17,6 @@ class LocationPickerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }

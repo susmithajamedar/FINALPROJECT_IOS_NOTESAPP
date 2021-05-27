0131dbf163d82a6a10e25f50b3b559b1052ee9f7
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-27 13:40:11 -0400

    Create NoteDetailView Controller
    Create NoteDetailView Controller

---
+++ note_avengers_iOS/note_avengers_iOS/NoteDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NoteDetailViewController.swift
+//  note_avengers_iOS
+//
+//  Created by Vijay Kumar Sevakula on 2021-05-27.
+//
+
+import UIKit
+
+class NoteDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

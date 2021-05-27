b33d68a6b878547c8dc1b8fd09352ee2a14d18aa
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-27 14:29:27 -0400

    Created NoteTableViewCell file
    Created NoteTableViewCell file

---
+++ note_avengers_iOS/note_avengers_iOS/NoteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoteTableViewCell.swift
+//  note_avengers_iOS
+//
+//  Created by Vijay Kumar Sevakula on 2021-05-27.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

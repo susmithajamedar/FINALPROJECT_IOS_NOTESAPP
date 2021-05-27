a394c49c4a8202dcbfeef62f2da0cde9c88d349a
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-27 16:28:49 -0400

    Functionality to Get Address Given a Latitude and longitude
    Functionality to Get Address Given a Latitude and longitude

--- note_avengers_iOS/note_avengers_iOS/LocationPickerViewController.swift
+++ note_avengers_iOS/note_avengers_iOS/LocationPickerViewController.swift
@@ -17,7 +17,7 @@ class LocationPickerViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-   
+    var currentLocation: CLLocation?
     func determineCurrentLocation(){
         locationManager = CLLocationManager()
         locationManager.delegate = self
@@ -31,6 +31,54 @@ class LocationPickerViewController: UIViewController {
             locationManager.startUpdatingLocation()
         }
     }
+    
+    func getAddress(location: CLLocation,handler: @escaping (String) -> Void)
+    {
+        self.currentLocation = location
+        let geoCoder = CLGeocoder()
+    
+        geoCoder.reverseGeocodeLocation(location, completionHandler:
+        { (placemarks, error) -> Void in
+            if let placemarks = placemarks
+            {
+                let pm = placemarks as [CLPlacemark]
+                if pm.count > 0
+                {
+                    let pm = placemarks[0]
+                  
+                    var addressString : String = ""
+                    if pm.subLocality != nil {
+                        addressString = addressString + pm.subLocality! + ", "
+                    }
+                    if pm.thoroughfare != nil {
+                        addressString = addressString + pm.thoroughfare! + ", "
+                    }
+                    if pm.locality != nil {
+                        addressString = addressString + pm.locality! + ", "
+                    }
+                    if pm.country != nil {
+                        addressString = addressString + pm.country! + ", "
+                    }
+                    if pm.postalCode != nil {
+                        addressString = addressString + pm.postalCode! + " "
+                    }
+                    
+                    
+                    handler(addressString)
+                }
+                
+            }
+           
+            
+            
+            
+           // Passing address back
+          
+        })
+    }
+    
+    
+    
 
 }
 

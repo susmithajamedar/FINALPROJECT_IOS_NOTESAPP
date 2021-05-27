db027dd751efc9999527bb1a324b2e45ebd52478
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-27 16:20:16 -0400

    Added Functionality to determine Current Location
    Added Functionality to determine Current Location or User Location

--- note_avengers_iOS/note_avengers_iOS/LocationPickerViewController.swift
+++ note_avengers_iOS/note_avengers_iOS/LocationPickerViewController.swift
@@ -10,13 +10,31 @@ import MapKit
 class LocationPickerViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var addressLabel: UILabel!
+    var locationManager =  CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        determineCurrentLocation()
         // Do any additional setup after loading the view.
     }
     
-
    
+    func determineCurrentLocation(){
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        self.mapView.showsUserLocation = true
+        self.mapView.delegate = self
+        
+        if CLLocationManager.locationServicesEnabled() {
+            //locationManager.startUpdatingHeading()
+            locationManager.startUpdatingLocation()
+        }
+    }
 
+}
+
+
+extension  LocationPickerViewController : MKMapViewDelegate, CLLocationManagerDelegate{
+    
 }

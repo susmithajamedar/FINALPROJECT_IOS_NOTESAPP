1151a3cc9e4f4dd01fe111e16ff972f29ed24440
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-27 00:04:38 -0400

    NS object class created

---
+++ note_avengers_iOS/NotesItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  NotesItem+CoreDataClass.swift
+//  note_avengers_iOS
+//
+//  Created by Vijay Kumar Sevakula on 2021-05-27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NotesItem)
+public class NotesItem: NSManagedObject {
+
+}

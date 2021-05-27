94f16a1c0cb90ca55e8856a4baf6979febd9e106
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-27 14:07:50 -0400

    Linked ActionItems in design to Code
    Added @IBOutlets and @IBAction function to action items

--- note_avengers_iOS/note_avengers_iOS/NotesListViewController.swift
+++ note_avengers_iOS/note_avengers_iOS/NotesListViewController.swift
@@ -1,29 +1,41 @@
 //
-//  NotesListViewController.swift
-//  note_avengers_iOS
+//  ViewController.swift
+//  NotesApp
 //
-//  Created by Vijay Kumar Sevakula on 2021-05-27.
-//
+//￼
 
 import UIKit
+import CoreData
+
 
 class NotesListViewController: UIViewController {
-
+    @IBOutlet weak var notesTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var sortSegment: UISegmentedControl!
+    
+    
+    var filtredNotes = [NotesItem]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+       
+        
+    }
+    @IBAction func addNote(_ sender: UIBarButtonItem) {
+        let vc = storyboard?.instantiateViewController(identifier: "NoteDetailViewController") as! NoteDetailViewController
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    @IBAction func sortSwitched(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+            self.filtredNotes = self.filtredNotes.sorted(by: {$0.title! < $1.title!})
+            
+            self.notesTableView.reloadData()
+        }else{
+            self.filtredNotes = self.filtredNotes.sorted(by: {$0.time > $1.time})
+            
+            self.notesTableView.reloadData()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

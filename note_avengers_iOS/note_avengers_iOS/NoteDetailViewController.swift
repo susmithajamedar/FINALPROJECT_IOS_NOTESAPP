1e7e49c6435f90b7d865d32533e87968f84c2963
susmithajamedar <84050055+susmithajamedar@users.noreply.github.com>
2021-05-28 00:13:55 -0400

    Added Navigation to NoteDetailItem
    Added Navigation to NoteDetailView
    Programatically and added save button

--- note_avengers_iOS/note_avengers_iOS/NoteDetailViewController.swift
+++ note_avengers_iOS/note_avengers_iOS/NoteDetailViewController.swift
@@ -20,6 +20,8 @@ class NoteDetailViewController: UIViewController {
     @IBOutlet weak var subtitleLabel: UITextField!
     @IBOutlet weak var noteBodyTextView: UITextView!
     @IBOutlet weak var locationButton: UIButton!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     var note : NotesItem?
     override func viewDidLoad() {
@@ -29,14 +31,79 @@ class NoteDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addLocationButtonTapped(_ button: UIButton) {
+        
+        
+        let vc = storyboard?.instantiateViewController(identifier: "LocationPickerViewController") as! LocationPickerViewController
+        
+        self.navigationController?.pushViewController(vc, animated: true)
     }
-    */
+    
+    @IBAction func saveButtonTapped(_ button: UIBarButtonItem) {
+       
+        let noteItem = NotesItem(context: self.context)
+        if let title = self.titleLabel.text{
+            let finalTitle = title.trimmingCharacters(in: .whitespacesAndNewlines)
+            if !finalTitle.isEmpty{
+                //note.title = finalTitle
+                noteItem.title = finalTitle
+            }else{
+                display_alert(msg_title: "Alert", msg_desc: "Note title is required", action_title: "OK")
+                return
+            }
+        }
+        
+        if let text = self.subtitleLabel.text{
+            let subject = text.trimmingCharacters(in: .whitespacesAndNewlines)
+            if !subject.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty{
+                noteItem.subject = subject
+            }else{
+                display_alert(msg_title: "Alert", msg_desc: "Note subject is required", action_title: "OK")
+                return
+            }
+        }
+        
+        if let text = self.noteBodyTextView.text{
+            let body = text.trimmingCharacters(in: .whitespacesAndNewlines)
+            if !body.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty{
+                noteItem.body = body
+            }else{
+                display_alert(msg_title: "Alert", msg_desc: "Note body is required", action_title: "OK")
+                return
+            }
+        }
+        
+       
+        
+        
+      
+       
+        if let note = self.note{
+            //self.note = noteItem
+            self.context.delete(note)
+        }else{
+          
+        }
+        
+        do{
+            try context.save()
+            self.navigationController?.popViewController(animated: true)
+        }catch let error {
+            print(error.localizedDescription)
+        }
+        
+        
+        
+    }
+    
+    func display_alert(msg_title : String , msg_desc : String ,action_title : String){
+        let ac = UIAlertController(title: msg_title, message: msg_desc, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: action_title, style: .default)
+        {
+            (result : UIAlertAction) -> Void in
+           // _ = self.navigationController?.popViewController(animated: true)
+        })
+        present(ac, animated: true)
+    }
 
 }
